de739811733204755ca2061f2aad49688f0fb387
Sidetrail <branchingtrails@gmail.com>
2018-06-08 21:12:04 -0500

    added an image gallery to the splash page and setup the react router links in the header bar

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,8 +1,29 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Header = () => {
 
-    return(<div>header</div>)
+    return(
+    <div className="headerLinks">
+        <Link to="/">
+            <label>HeaderImage</label> 
+        </Link>
+        <Link to="/info">
+            <label>Info</label> 
+       </Link>
+       <Link to="/code">
+            <label>Code</label> 
+       </Link>
+       <Link to="/videos">
+            <label>Videos</label> 
+       </Link>
+       <Link to="/images">
+            <label>Images</label> 
+       </Link>
+       <Link to="/contact">
+            <label>Contact</label>
+       </Link> 
+    </div>)
 }
 
 export default Header;
--- src/components/Splash.js
+++ src/components/Splash.js
@@ -1,8 +1,25 @@
 import React from 'react';
+import ImageGallery from 'react-image-gallery';
+import "react-image-gallery/styles/css/image-gallery.css";
 
 const Splash = () => {
 
-    return(<div>splash</div>)
+    const images = [
+        {original:"https://i.imgur.com/ppqan5G.jpg"},
+        {original:"https://c1.staticflickr.com/5/4403/36388952880_c9d523338f_o.jpg"}
+    ]
+
+    return(
+    <div className="imageGallery">
+        <ImageGallery 
+            items={images} 
+            autoPlay={true} 
+            showThumbnails={false} 
+            showFullscreenButton={false}
+            showPlayButton={false}
+            slideInterval={5000}
+        />
+    </div>)
 }
 
 export default Splash;
