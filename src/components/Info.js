78acc067420b8cc90e43608acb10189aa691038a
Sidetrail <branchingtrails@gmail.com>
2018-06-21 20:37:31 -0500

    added some content to the info page and styled some of the content to get an idea of how the page will be layed out

--- src/components/Info.js
+++ src/components/Info.js
@@ -8,21 +8,93 @@ const Info = () => (
         <div htmlFor="name" className="infoNameTitle">
           John Williams
         </div>
-        <div className="infoIntroText">Intro text</div>
+        <div className="infoIntroText">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce viverra
+          risus sit amet urna mollis, ut porta arcu interdum. Aliquam eleifend
+          tortor sem, vitae facilisis erat facilisis in. Nunc arcu justo, tempor
+          a mollis ut, laoreet iaculis tellus. Vivamus at porttitor sapien.
+          Quisque finibus nunc a dolor volutpat, a laoreet purus pharetra.
+          Vestibulum varius, nunc non fermentum consectetur, urna lorem viverra
+          nisl, sit amet vulputate ex leo eget tortor. Curabitur tempor, ex ac
+          euismod hendrerit, lorem enim ultricies nulla, at eleifend elit ligula
+          ut lorem. Vestibulum ante ipsum primis in faucibus orci luctus et
+          ultrices posuere cubilia Curae; Integer libero nibh, molestie
+          venenatis convallis in, ultrices sit amet erat. Aliquam sit amet
+          tempus erat. Donec quis neque vitae metus vestibulum interdum. Proin
+          purus libero, sodales id ornare vel, ornare vel nisi. Integer viverra
+          felis non porta mattis. Phasellus vitae lorem eu ipsum fermentum porta
+          nec ut risus. Vestibulum molestie risus ac risus pretium, ac blandit
+          quam bibendum. Suspendisse lobortis odio sapien, non convallis magna
+          porta quis.
+        </div>
       </div>
     </div>
-    <div>
+    <div className="infoShortDesc">
       <div>
-        <div>Videos</div>
-        <div>Video text</div>
+        <div className="infoDescTitle">Videos</div>
+        <div className="infoDescBody">
+          Video text Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+          Fusce viverra risus sit amet urna mollis, ut porta arcu interdum.
+          Aliquam eleifend tortor sem, vitae facilisis erat facilisis in. Nunc
+          arcu justo, tempor a mollis ut, laoreet iaculis tellus. Vivamus at
+          porttitor sapien. Quisque finibus nunc a dolor volutpat, a laoreet
+          purus pharetra. Vestibulum varius, nunc non fermentum consectetur,
+          urna lorem viverra nisl, sit amet vulputate ex leo eget tortor.
+          Curabitur tempor, ex ac euismod hendrerit, lorem enim ultricies nulla,
+          at eleifend elit ligula ut lorem. Vestibulum ante ipsum primis in
+          faucibus orci luctus et ultrices posuere cubilia Curae; Integer libero
+          nibh, molestie venenatis convallis in, ultrices sit amet erat. Aliquam
+          sit amet tempus erat. Donec quis neque vitae metus vestibulum
+          interdum. Proin purus libero, sodales id ornare vel, ornare vel nisi.
+          Integer viverra felis non porta mattis. Phasellus vitae lorem eu ipsum
+          fermentum porta nec ut risus. Vestibulum molestie risus ac risus
+          pretium, ac blandit quam bibendum. Suspendisse lobortis odio sapien,
+          non convallis magna porta quis.
+        </div>
       </div>
       <div>
-        <div>Programming</div>
-        <div>coding text</div>
+        <div className="infoDescTitle">Programming</div>
+        <div className="infoDescBody">
+          coding text Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+          Fusce viverra risus sit amet urna mollis, ut porta arcu interdum.
+          Aliquam eleifend tortor sem, vitae facilisis erat facilisis in. Nunc
+          arcu justo, tempor a mollis ut, laoreet iaculis tellus. Vivamus at
+          porttitor sapien. Quisque finibus nunc a dolor volutpat, a laoreet
+          purus pharetra. Vestibulum varius, nunc non fermentum consectetur,
+          urna lorem viverra nisl, sit amet vulputate ex leo eget tortor.
+          Curabitur tempor, ex ac euismod hendrerit, lorem enim ultricies nulla,
+          at eleifend elit ligula ut lorem. Vestibulum ante ipsum primis in
+          faucibus orci luctus et ultrices posuere cubilia Curae; Integer libero
+          nibh, molestie venenatis convallis in, ultrices sit amet erat. Aliquam
+          sit amet tempus erat. Donec quis neque vitae metus vestibulum
+          interdum. Proin purus libero, sodales id ornare vel, ornare vel nisi.
+          Integer viverra felis non porta mattis. Phasellus vitae lorem eu ipsum
+          fermentum porta nec ut risus. Vestibulum molestie risus ac risus
+          pretium, ac blandit quam bibendum. Suspendisse lobortis odio sapien,
+          non convallis magna porta quis.
+        </div>
       </div>
       <div>
-        <div>Pictures</div>
-        <div>picture text</div>
+        <div className="infoDescTitle">Pictures</div>
+        <div className="infoDescBody">
+          picture text Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+          Fusce viverra risus sit amet urna mollis, ut porta arcu interdum.
+          Aliquam eleifend tortor sem, vitae facilisis erat facilisis in. Nunc
+          arcu justo, tempor a mollis ut, laoreet iaculis tellus. Vivamus at
+          porttitor sapien. Quisque finibus nunc a dolor volutpat, a laoreet
+          purus pharetra. Vestibulum varius, nunc non fermentum consectetur,
+          urna lorem viverra nisl, sit amet vulputate ex leo eget tortor.
+          Curabitur tempor, ex ac euismod hendrerit, lorem enim ultricies nulla,
+          at eleifend elit ligula ut lorem. Vestibulum ante ipsum primis in
+          faucibus orci luctus et ultrices posuere cubilia Curae; Integer libero
+          nibh, molestie venenatis convallis in, ultrices sit amet erat. Aliquam
+          sit amet tempus erat. Donec quis neque vitae metus vestibulum
+          interdum. Proin purus libero, sodales id ornare vel, ornare vel nisi.
+          Integer viverra felis non porta mattis. Phasellus vitae lorem eu ipsum
+          fermentum porta nec ut risus. Vestibulum molestie risus ac risus
+          pretium, ac blandit quam bibendum. Suspendisse lobortis odio sapien,
+          non convallis magna porta quis.
+        </div>
       </div>
     </div>
   </div>
