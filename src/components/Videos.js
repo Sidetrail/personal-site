da9bc610de72aff70246b31a1978c82e8726d2bb
Sidetrail <branchingtrails@gmail.com>
2018-06-22 21:52:23 -0500

    added some content to the video page

--- src/components/Videos.js
+++ src/components/Videos.js
@@ -1,5 +1,13 @@
 import React from 'react';
 
-const Videos = () => <div>videos</div>;
+const Videos = () => (
+  <div>
+    <div className="featuredVideoContainer">
+      <div className="featuredVideo">featured video</div>
+      <div className="featuredVideoDesc">featured video desc</div>
+    </div>
+    <div className="videoList">last few videos</div>
+  </div>
+);
 
 export default Videos;
